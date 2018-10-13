8a29d5a8e447526cfaf1043c732c2967153d6c44
user912249 <user912249@74-80-245-13.ic2net.net>
2018-10-12 17:58:08 -0700

    Autolayout teal de total e teal de compras

---
+++ TaianeVinicius/PurchaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PurchaseViewController.swift
+//  TaianeVinicius
+//
+//  Created by user912249 on 10/12/18.
+//  Copyright © 2018 TaianeVinicius. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
